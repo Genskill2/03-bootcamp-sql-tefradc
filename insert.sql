c1237ef343baaa9ba2a3e056667f875594ceabda
Tedy Francy <tefradc350@gmail.com>
2021-06-18 14:09:02 +0530

    adding2

--- insert.sql
+++ insert.sql
@@ -1,122 +1,55 @@
-insert into publisher(name, country)
-values('PHI', 'India');
-
-insert into publisher(name, country)
-values('Harper', 'USA');
-
-insert into publisher(name, country)
-values('GCP', 'USA');
-
-insert into publisher(name, country)
-values('Avery', 'USA');
-
-insert into publisher(name, country)
-values('Del Rey', 'UK');
-
-insert into publisher(name, country)
-values('Vintage', 'UK');
+insert into publisher values(1,"PHI","India");
+insert into publisher values(2,"Harper","USA");
+insert into publisher values(3,"GCP","USA");
+insert into publisher values(4,"Avery","USA");
+insert into publisher values(5,"Del Rey","UK");
+insert into publisher values(6,"Vintage","UK");
 
 
-insert into subjects(name)
-values('C');
-
-insert into subjects(name)
-values('UNIX');
-
-insert into subjects(name)
-values('Technology');
-
-insert into subjects(name)
-values('Go');
-
-insert into subjects(name)
-values('Science Fiction');
-
-insert into subjects(name)
-values('Productivity');
-
-insert into subjects(name)
-values('Psychology');
-
-insert into subjects(name)
-values('Politics');
-
-insert into subjects(name)
-values('History');
 
 
-insert into books(title, publisher)
-values('The C Programming Language', 1);
-
-insert into books(title, publisher)
-values('The Go Programming Language', 1);
-
-insert into books(title, publisher)
-values('The UNIX Programming Environment', 1);
-
-insert into books(title, publisher)
-values('Cryptonomicon', 2);
-
-insert into books(title, publisher)
-values('Deep Work', 3);
-
-insert into books(title, publisher)
-values('Atomic Habits', 4);
-
-insert into books(title, publisher)
-values('The City and The City', 5);
-
-insert into books(title, publisher)
-values('The Great War for Civilisation', 6);
+insert into books values(1,"The C Programming Language",1);
+insert into books values(2,"The Go Programming Language",1);
+insert into books values(3,"The UNIX Programming Environment",1);
+insert into books values(4,"Cryptonomicon",2);
+insert into books values(5,"Deep Work",3);
+insert into books values(6,"Atomic Habits",4);
+insert into books values(7,"The City and The City",5);
+insert into books values(8,"The Great War for Civilisation",6);
 
 
-insert into books_subjects(book, subject)
-values(1, 1);
 
-insert into books_subjects(book, subject)
-values(1, 2);
 
-insert into books_subjects(book, subject)
-values(1, 3);
 
-insert into books_subjects(book, subject)
-values(2, 3);
+insert into subjects values(1,"C");
+insert into subjects values(2,"UNIX");
+insert into subjects values(3,"Technology");
+insert into subjects values(4,"Go");
+insert into subjects values(5,"Science Fiction");
+insert into subjects values(6,"Productivity");
+insert into subjects values(7,"Psychology");
+insert into subjects values(8,"Politics");
+insert into subjects values(9,"History");
 
-insert into books_subjects(book, subject)
-values(2, 4);
 
-insert into books_subjects(book, subject)
-values(3, 2);
 
-insert into books_subjects(book, subject)
-values(3, 3);
 
-insert into books_subjects(book, subject)
-values(4, 3);
 
-insert into books_subjects(book, subject)
-values(4, 5);
 
-insert into books_subjects(book, subject)
-values(5, 3);
-
-insert into books_subjects(book, subject)
-values(5, 6);
-
-insert into books_subjects(book, subject)
-values(6, 6);
-
-insert into books_subjects(book, subject)
-values(6, 7);
-
-insert into books_subjects(book, subject)
-values(7, 5);
-
-insert into books_subjects(book, subject)
-values(7, 8);
-
-insert into books_subjects(book, subject)
-values(8, 8);
-
-insert into books_subjects(book, subject)
-values(8, 9);
+insert into books_subjects values(1,1);
+insert into books_subjects values(1,2);
+insert into books_subjects values(1,3);
+insert into books_subjects values(2,4);
+insert into books_subjects values(2,3);
+insert into books_subjects values(3,2);
+insert into books_subjects values(3,3);
+insert into books_subjects values(4,3);
+insert into books_subjects values(4,5);
+insert into books_subjects values(5,3);
+insert into books_subjects values(5,6);
+insert into books_subjects values(6,6);
+insert into books_subjects values(6,7);
+insert into books_subjects values(7,5);
+insert into books_subjects values(7,8);
+insert into books_subjects values(8,8);
+insert into books_subjects values(8,9);
